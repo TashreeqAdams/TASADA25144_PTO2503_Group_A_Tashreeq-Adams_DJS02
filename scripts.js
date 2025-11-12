ef2cf4c9ebedc6bcbe736da902fbf2ef0fc2c6d3
Tashreeq Adams <tashreeqadams8@gmail.com>
2025-11-12 20:07:32 +0200

    Added custom HTML element

--- scripts.js
+++ scripts.js
@@ -1,57 +1,88 @@
 import { podcasts, genres, seasons } from "./data.js";
 import { daysAgo } from "./dateConverter.js";
 
-/**
- * Reads through the array and adds the thumbnails with its information into the DOM.
- *
- * Finds the podcast image, title, season count, genres and last updated time in the 'podcast' array,
- * creates the thumbnail elements and appends each thumbnail to the #thumbnail-container.
- *
- * @param {Array<Object>} podcasts - Array of podcast objects.
- * @param {Array<Object>} genres - Array of genre objects used to match genre IDs and titles.
- */
+class podcastPreview extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow.apply({ mode: "open" });
 
-podcasts.forEach((podcast) => {
-  /**
-   * Renders a string for the genres of the current thumbnail.
-   *
-   * For each 'podcast.genre' an id is collected and matched with
-   * a corresponding id from the genre objects in the genres array.
-   * If no match the default genre is "unknown".
-   *
-   * @type {string}
-   */
-  const genreTitles = podcast.genres
-    .map((genreId) => {
-      const genre = genres.find((g) => g.id === genreId);
-      return genre ? genre.title : "Unknown";
-    })
-    .join(", ");
+    this.shadowRoot.innerHTML = `<style>
+.thumbnail {
+  width: 100%;
+  max-width: 380px;
+  height: 100%;
+  background-color: #202648;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: flex-start;
+  padding-top: 10px;
+  box-shadow: 2px 2px 10px #353342;
+  gap: 8px;
+}
 
-  /**
-   * Splits the joined comma-separated genres into an array.
-   */
-  const splitTitles = genreTitles.split(",");
+.podcast-img {
+  width: 90%;
+  border-radius: 10px;
+  display: block;
+  margin: 0 auto;
+}
 
-  const thumbnail = document.getElementById("thumbnail-container");
+.seasonDetails {
+  height: 15px;
+  display: flex;
+  align-items: center;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-left: 20px;
+  padding-top: 10px;
+}
 
-  const podcastThumbnail = document.createElement("div");
-  podcastThumbnail.classList.add("thumbnail");
+.seasonDetails i {
+  padding-right: 10px;
+  color: #febf0e;
+}
 
-  // Sets the data in the thumbnail to be displayed
-  podcastThumbnail.innerHTML = `
-      <img class="podcast-img" src="${podcast.image}" alt="${podcast.title}" />
-      <h1 class="podcast-title">${podcast.title}</h1>
-      <div class="seasonDetails">
-        <i class="fa fa-calendar"></i>
-        <p>${podcast.seasons} seasons</p>
-      </div>
-        <div class="podcast-genres">${splitTitles
-          .map((title) => `<p>${title.trim()}</p>`)
-          .join("")}</div>
-        <p class="podcast-update">Updated ${daysAgo(podcast.updated)}</p>
-    `;
+.seasonDetails,
+.podcast-genres,
+.podcast-update {
+  font-size: 17px;
+}
 
-  // Appends the thumbnail to the #thumbnail-container.
-  thumbnail.appendChild(podcastThumbnail);
-});
+.podcast-genres,
+.podcast-update {
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-left: 20px;
+  padding-top: 10px;
+}
+
+.podcast-title {
+  font-size: 22px;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-left: 20px;
+  padding-top: 10px;
+  color: #febf0e;
+}
+
+</style>
+
+<img class="podcast-img" src="${podcast.image}" alt="${podcast.title}" />
+<h1 class="podcast-title">${podcast.title}</h1>
+<div class="seasonDetails">
+<i class="fa fa-calendar"></i>
+<p>${podcast.seasons} seasons</p>
+</div>
+<div class="podcast-genres">${splitTitles
+      .map((title) => `<p>${title.trim()}</p>`)
+      .join("")}</div>
+<p class="podcast-update">Updated ${daysAgo(podcast.updated)}</p>
+
+`;
+  }
+}
+
+customElements.define("podcast-preview", podcastPreview);
